87bdf9e2ac9aa61b70873edec3e19f59e162a2a8
adewolff <30436922+adewolff@users.noreply.github.com>
2018-05-22 14:45:52 -0700

    Created shinyapp infrastructure. Close #1. Close #2

---
+++ scripts/ui.R
@@ -0,0 +1,43 @@
+shinyUI(navbarPage("College Finder",
+                   
+
+# about panel -------------------------------------------------------------
+
+                   tabPanel("About",
+                            titlePanel("About College Finder"),
+                            mainPanel("test")
+                     
+                   ), # End of tabPanel
+                   
+
+# map panel ---------------------------------------------------------------
+
+                   tabPanel("Map",
+                            titlePanel("Geographic map of colleges"),
+                            sidebarLayout(
+                              sidebarPanel(
+                                "stuff goes here"
+                              ), # End of sideparPanel
+                              mainPanel(
+                                "More stuff goes here"
+                              ) # End of mainPanel
+                            ) # End of sidebarLayout
+                   ), # End of tabPanel
+
+
+# graph panel -------------------------------------------------------------
+
+                   tabPanel("graph",
+                            titlePanel("Graph"),
+                            sidebarLayout(
+                              sidebarPanel(
+                                "stuff goes here"
+                              ), #end of sidebarPanel
+                              mainPanel(
+                                "More stuff goes here"
+                              ) # End of mainPanel
+                            ) # End of sidebarLayout
+                            
+                  ) # End of tabPanel
+  
+)) # End of shinyUI, navbarPage
